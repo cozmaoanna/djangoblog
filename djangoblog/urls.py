ea1565205486ce42e0809934ab9b17aa5ab5ab61
Oanacozma <cozmaoanna@yahoo.com>
2017-11-17 11:28:31 +0000

    image update

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from blog.views import blogposts
 from accounts import urls as accounts_urls
 from blog import urls as blog_urls
+from .settings import MEDIA_ROOT
+from django.views import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', blogposts, name="index"),
     url(r'^accounts/' ,include(accounts_urls)),
     url(r'^blog/' ,include(blog_urls)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 
 ]
 # """djangoblog URL Configuration
